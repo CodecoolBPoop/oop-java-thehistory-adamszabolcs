b02c5590a6df68e4a919957a0913b53958faf305
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-12 09:18:21 +0200

    Created array clear method,can replace words, and delete words from array

--- src/main/java/com/codecool/thehistory/TheHistoryArray.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArray.java
@@ -12,34 +12,62 @@ public class TheHistoryArray implements TheHistory {
     @Override
     public void add(String text) {
         //TODO: check the TheHistory interface for more information
-        String[] wordsAdded = Arrays.copyOf(wordsArray, wordsArray.length()+1);
-        wordsAdded[0] = text;
+        String[] parts = text.split("\\s*(\\s|,)\\s*");
+        wordsArray = Arrays.copyOf(wordsArray, parts.length);
+        System.arraycopy(parts, 0, wordsArray, 0, parts.length);
     }
 
     @Override
     public void removeWord(String wordToBeRemoved) {
         //TODO: check the TheHistory interface for more information
+        int counter = 0;
+        for (String word : wordsArray) {
+            if (word.equals(wordToBeRemoved)){
+                counter++;
+            }
+        }
+        String[] arrayWithoutRemovedWord = new String[wordsArray.length-counter];
+        int indexCounter = 0;
+        for (String word : wordsArray) {
+            if (word.equals(wordToBeRemoved)) {
+                continue;
+            } else {
+                arrayWithoutRemovedWord[indexCounter] = word;
+                indexCounter++;
+            }
+        }
     }
 
     @Override
     public int size() {
         //TODO: check the TheHistory interface for more information
-        return 0;
+        return wordsArray.length;
     }
 
     @Override
     public void clear() {
         //TODO: check the TheHistory interface for more information
+        wordsArray = new String[0];
     }
 
     @Override
     public void replaceOneWord(String from, String to) {
         //TODO: check the TheHistory interface for more information
+        for (int i = 0; i < wordsArray.length; i++) {
+            if (wordsArray[i].equals(from)) {
+                wordsArray[i] = to;
+            }
+        }
     }
 
     @Override
     public void replaceMoreWords(String[] fromWords, String[] toWords) {
         //TODO: check the TheHistory interface for more information
+        if (fromWords.length == toWords.length) {
+            for (int i = 0; i < wordsArray.length; i++) {
+
+            }
+        }
     }
 
     @Override
